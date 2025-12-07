fc1a23c8db650a97d7cb92f8ec40af3379f1641f
thung4974-a11y <thung4974@gmail.com>
2025-12-07 22:52:32 +0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -173,7 +173,8 @@ def calculate_grade(score):
         s = float(score)
     except Exception:
         s = 0.0
-    if s >= 8.5: return 'Giỏi'
+    if s >= 9.5: return 'Xuất sắc'
+    elif s >= 8.5: return 'Giỏi'
     elif s >= 7.0: return 'Khá'
     elif s >= 5.5: return 'Trung bình'
     elif s >= 4.0: return 'Yếu'
@@ -1192,6 +1193,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
